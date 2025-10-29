7aebaec6c004e5fd49f6f87a043437710bbb0318
momenhaitham <momenhaithamcod12@gmail.com>
2025-10-30 00:18:13 +0300

    first comit to development

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    
     return ScreenUtilInit(
       designSize: Size(375, 812),
       child: MaterialApp(
